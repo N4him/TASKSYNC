123ab713cadb05df9b5ec1b31b82d28c9abcd209
Steban01 <167812271+N4him@users.noreply.github.com>
2024-09-06 11:37:31 -0500

    Fix: register with hash and logic login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authenticateUser = require('../middleware/authMiddleware'); 
 
-router.post('/users', userController.createUser);
-router.get('/users', userController.getUsers);
+router.post('/register', userController.createUser);
+router.get('/login', userController.getUsers);
 
 module.exports = router;

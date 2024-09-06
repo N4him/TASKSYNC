264063f250b0a0e748fabc3d58ac7e924b592f38
Johandarkness <70907376+Johandarkness@users.noreply.github.com>
2024-09-05 21:14:31 -0500

    sign-up

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { LandingPage } from "@/components/component/landing-page";
+import { LandingPage } from "./pages/landing-page";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[5px_1fr_5px] items-center justify-items-center  font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <LandingPage/>
       </main>

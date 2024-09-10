8300bb3bd29d63b402239fbcba8014c698bf0e7d
Ironwolf1212 <velasco.miguel@correounivalle.edu.co>
2024-09-10 12:24:06 -0500

    Fixed some errors in the integration process

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,16 +6,24 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Name is required']
+    required: [true,  
+ 'Name is required']
   },
   email: {
     type: String,
-    required: [true, 'Email is required'],
+    required: [true, 'Email is required'],  
+
     unique: true
   },
   password: {
     type: String,
     required: [true, 'Password is required']
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'  
+
   }
 });
 
